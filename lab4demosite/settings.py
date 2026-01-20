eb2a1b6c04461c265ef94b386c8c2f71f25f925d
Adrian Kunz <a.kunz@uni-kassel.de>
2026-01-20 12:49:40 +0100

    fix: Settings

--- lab4demosite/settings.py
+++ lab4demosite/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$!&!hzlvme+ixw*5t=&&4h4^mq#f=!=w%^aikta7mvxuf*2586'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$!&!hzlvme+ixw*5t=&&4h4^mq#f=!=w%^aikta7mvxuf*2586')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
